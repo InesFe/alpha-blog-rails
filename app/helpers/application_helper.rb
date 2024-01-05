38182d7d69959987fc758758e649a7444cf0ecb5
Inès Fe <ines.fevrier@gmail.com>
2024-01-05 16:22:58 +0100

    page user articles created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-    def bootstrap_class_for(flash_type)
+    
+  def bootstrap_class_for(flash_type)
         case flash_type
         when 'success'
           'success'
@@ -12,5 +13,12 @@ module ApplicationHelper
         else
           flash_type.to_s
         end
+    end
+
+    def gravatar_for(user, options = { size: 200})
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      size = options[:size]
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      image_tag(gravatar_url, alt: user.username, class: "img-circle")
     end
 end
