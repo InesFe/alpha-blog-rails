abcec03eeffd14bd149c7faf4bba8c64b09af74a
Inès Fe <ines.fevrier@gmail.com>
2024-01-11 11:40:16 +0100

    sessions controler not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,19 @@ class SessionsController < ApplicationController
     end
 
     def create 
-        render 'new'
+        #if params[:session].present? && params[:session][:email].present? && params[:session][:password].present?
+            user = User.find_by(email: params[:session][:email].downcase)
+        
+            if user && user.authenticate(params[:session][:password])
+                flash[:success] = "Félicitations pour cette connexion"
+                session[:user_id] = user.id 
+                redirect_to user_path(user)
+        
+            else
+                flash.now[:danger] = "Identifiants incorrects"
+                render 'new'
+            end
+        #end
     end
 
     def destroy
