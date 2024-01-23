95576086b4122674bcc9d6d1de13e925e34b8dcc
Inès Fe <ines.fevrier@gmail.com>
2024-01-23 17:25:00 +0100

    admin et articles ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,7 +58,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-        if current_user != @article.user 
+        if current_user != @article.user and current_user.admin? == false
             flash[:danger] = "You have no power here! Not your article."
             redirect_to root_path
         end
