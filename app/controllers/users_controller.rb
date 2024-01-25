ee3255cc570d55a12db5abf454e89849123fe0ad
Inès Fe <ines.fevrier@gmail.com>
2024-01-25 18:02:52 +0100

    admin fonctionnalités ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController 
-    before_action :set_user, only: [:edit, :update, :show]
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
     before_action :require_same_user, only: [:edit, :update, :destroy] 
+    before_action :require_admin, only: [:destroy] 
     
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -40,6 +41,12 @@ class UsersController < ApplicationController
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 8)
     end
 
+    def destroy
+        @user.destroy
+        flash[:danger] = "Compte utilisateur et articles supprimés buddy"
+        redirect_to users_path
+    end
+
     private
 
     def set_user
@@ -51,9 +58,16 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if current_user != @user
+        if current_user != @user and current_user.admin? == false
             flash[:danger] = "Vous n'êtes pas le bon utilisateur!"
             redirect_to root_path
+        end
+    end
+
+    def require_admin
+        if logged_in? and current_user.admin? == false
+            flash[:danger] = "Action impossible si vous n'êtes pas admin"
+            redirect_to users_path
         end
     end
 end
